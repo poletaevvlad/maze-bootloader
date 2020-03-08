0f3ec18e2d8c226676c70e06efe35164bc5bcb6a
Vlad Poletaev <poletaev.vladislav@gmail.com>
2020-03-09 00:03:09 +0300

    Generating maze

--- bootloader.asm
+++ bootloader.asm
@@ -7,7 +7,7 @@ bits 16
 
     mov ax, 0x07C0
     mov ds, ax
-    mov [ds:0], word 0x6F84
+    mov [ds:0], word 0x6F87
 
     mov ax, 0x07E0
     mov ss, ax
@@ -39,6 +39,12 @@ bits 16
 
         call get_neighbours
 
+        cmp dx, 0
+        jne .iteration
+        add sp, 16
+        jmp _loop
+
+        .iteration:
         push ax
 
         call gen_random
@@ -47,6 +53,7 @@ bits 16
 
         xor al, al
         xchg ah, al
+        ; mov ax, 0
 
         inc ax
         shl ax, 2
@@ -58,6 +65,13 @@ bits 16
         call connect
 
         add sp, 16
+
+        push bx
+        push ax
+        push dx
+        push cx
+
+    jmp _loop
 
     _end:
 
@@ -114,66 +128,69 @@ connect:
     add ax, cx
     shr ax, 1
 
-    jmp .end
+    jmp .connect_end
 
     .connect_same_row:
     add bx, dx
     shr bx, 1
 
-    .end:
+    .connect_end:
     call set_cell
     popa
+    ret
+
+
+;add_neighbour(ax, bx, si)
+add_neighbour:
+    call get_offset
+    mov cl, [es:di]
+    cmp cl, 0x00
+    jne .add_neighbour_end
+
+    mov [ss:si], ax
+    mov [ss:si + 2], bx
+    add si, 4
+    inc dx
+
+    .add_neighbour_end:
     ret
 
 
 ;get_neighbours(ax, bx, si) -> dx
 get_neighbours:
     push cx
-
     xor dx, dx
 
     cmp bx, 0
     je .skip_left_column
 
-    mov [ss:si], ax
-    mov cx, bx
-    sub cx, 2
-    mov [ss:si + 2], cx
-    add si, 4
-    inc dx
+    sub bx, 2
+    call add_neighbour
+    add bx, 2
     .skip_left_column:
 
     cmp ax, 0
     je .skip_top_row
 
-    mov cx, ax
-    sub cx, 2
-    mov [ss:si], cx
-    mov [ss:si + 2], bx
-    add si, 4
-    inc dx
+    sub ax, 2
+    call add_neighbour
+    add ax, 2
     .skip_top_row:
 
     cmp bx, (SCREEN_WIDTH - 2)
-    je .skip_right_column
+    jae .skip_right_column
 
-    mov [ss:si], ax
-    mov cx, bx
-    add cx, 2
-    mov [ss:si + 2], cx
-    add si, 4
-    inc dx
+    add bx, 2
+    call add_neighbour
+    sub bx, 2
     .skip_right_column:
 
     cmp ax, (SCREEN_HEIGHT - 2)
-    je .skip_bottom_row
+    jae .skip_bottom_row
 
-    mov cx, ax
-    add cx, 2
-    mov [ss:si], cx
-    mov [ss:si + 2], bx
-    add si, 4
-    inc dx
+    add ax, 2
+    call add_neighbour
+    sub ax, 2
     .skip_bottom_row:
 
     pop cx
